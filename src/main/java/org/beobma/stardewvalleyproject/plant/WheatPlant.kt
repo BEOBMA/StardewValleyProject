b81387750221cbfe691057d98fa3e6f088e95111
BEOBMA <dingxiao19@gmail.com>
2024-08-05 22:56:08 +0900

    refactor: 각종 작물 클래스에 Plant 클래스를 상속받아 사용하도록 변경

---
+++ src/main/java/org/beobma/stardewvalleyproject/plant/WheatPlant.kt
@@ -0,0 +1,29 @@
+package org.beobma.stardewvalleyproject.plant
+
+import net.kyori.adventure.text.Component
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class WheatPlant : Plant("밀", 4, 1) {
+    override fun getSeedItem(): ItemStack {
+        return ItemStack(Material.WHEAT_SEEDS, 1).apply {
+            itemMeta = itemMeta.apply {
+                displayName(Component.text("밀 씨앗"))
+                lore(
+                    listOf(Component.text("성장하려면 4일이 소요됩니다."))
+                )
+            }
+        }
+    }
+
+    override fun getHarvestItem(): ItemStack {
+        return ItemStack(Material.WHEAT, 1).apply {
+            itemMeta = itemMeta.apply {
+                displayName(Component.text("밀"))
+                lore(
+                    listOf(Component.text("널리 재배되는 곡물 중 하나입니다."))
+                )
+            }
+        }
+    }
+}
