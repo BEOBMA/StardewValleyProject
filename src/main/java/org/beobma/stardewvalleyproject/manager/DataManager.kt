2ba1d93a8fc8299196b51545883b80df867ffde3
BEOBMA <dingxiao19@gmail.com>
2024-08-06 19:32:44 +0900

    refactor: 직렬화 및 역직렬화 어뎁터 구현.

--- src/main/java/org/beobma/stardewvalleyproject/manager/DataManager.kt
+++ src/main/java/org/beobma/stardewvalleyproject/manager/DataManager.kt
@@ -1,13 +1,16 @@
 package org.beobma.stardewvalleyproject.manager
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import com.google.gson.*
 import org.beobma.stardewvalleyproject.StardewValley
 import org.beobma.stardewvalleyproject.data.GameData
+import org.beobma.stardewvalleyproject.plant.Plant
+import org.bukkit.Bukkit
+import org.bukkit.block.Block
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 import java.io.IOException
+import java.lang.reflect.Type
 
 interface DataHandler {
     fun saveData()
@@ -17,10 +20,15 @@ interface DataHandler {
 
 class DefaultDataHandler : DataHandler {
     private val folder: File = File(StardewValley.instance.dataFolder, "/data")
-    private val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+    private val gson: Gson = GsonBuilder()
+        .registerTypeAdapter(Plant::class.java, PlantAdapter())
+        .registerTypeAdapter(Block::class.java, BlockTypeAdapter())
+        .enableComplexMapKeySerialization() // 복합 키 직렬화 활성화
+        .setPrettyPrinting()
+        .create()
 
     companion object {
-        var gameData = GameData(6, 0, Season.Spring, 1, mutableListOf(), mutableListOf())
+        var gameData = GameData(6, 0, Season.Spring, 1, mutableListOf(), mutableListOf(), mutableMapOf())
     }
 
     override fun saveData() {
@@ -31,7 +39,7 @@ class DefaultDataHandler : DataHandler {
             }
             val dataFile = File(folder, "data.json")
             FileWriter(dataFile).use { writer ->
-                gson.toJson(gameData, writer)
+                writer.write(gson.toJson(gameData))
             }
         } catch (e: IOException) {
             logMessage("Failed to save data: ${e.message}")
@@ -48,7 +56,17 @@ class DefaultDataHandler : DataHandler {
             val dataFile = File(folder, "data.json")
             if (dataFile.exists()) {
                 FileReader(dataFile).use { reader ->
-                    gameData = gson.fromJson(reader, GameData::class.java)
+                    val loadedData = gson.fromJson(reader, GameData::class.java)
+                    if (loadedData is GameData) {
+                        gameData = loadedData
+                        StardewValley.instance.loggerMessage("$gameData")
+                        val minecraftTime = (gameData.hour * 1000) + ((gameData.minute / 60.0) * 1000).toInt()
+                        val world = Bukkit.getWorld("world")
+                        world?.time = minecraftTime.toLong()
+                    } else {
+                        logMessage("Loaded data is not valid, initializing with default values.")
+                        gameData = GameData(6, 0, Season.Spring, 1, mutableListOf(), mutableListOf(), mutableMapOf())
+                    }
                 }
             }
         } catch (e: IOException) {
@@ -62,6 +80,38 @@ class DefaultDataHandler : DataHandler {
         StardewValley.instance.loggerMessage(message)
     }
 }
+
+class PlantAdapter : JsonSerializer<Plant>, JsonDeserializer<Plant> {
+    override fun serialize(src: Plant, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
+        val jsonObject = JsonObject()
+        jsonObject.addProperty("type", src::class.java.name)
+        jsonObject.add("properties", context.serialize(src))
+        return jsonObject
+    }
+
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Plant {
+        val jsonObject = json.asJsonObject
+        val type = Class.forName(jsonObject.get("type").asString)
+        return context.deserialize(jsonObject.get("properties"), type)
+    }
+}
+
+class BlockTypeAdapter : JsonSerializer<Block>, JsonDeserializer<Block> {
+    override fun serialize(src: Block, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
+        return JsonPrimitive("${src.world.name},${src.x},${src.y},${src.z}")
+    }
+
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Block {
+        val parts = json.asString.split(",")
+        val world = Bukkit.getWorld(parts[0])
+            ?: throw JsonParseException("World not found: ${parts[0]}")
+        val x = parts[1].toIntOrNull() ?: throw JsonParseException("Invalid X coordinate: ${parts[1]}")
+        val y = parts[2].toIntOrNull() ?: throw JsonParseException("Invalid Y coordinate: ${parts[2]}")
+        val z = parts[3].toIntOrNull() ?: throw JsonParseException("Invalid Z coordinate: ${parts[3]}")
+        return world.getBlockAt(x, y, z)
+    }
+}
+
 
 
 class DataManager(private val handler: DataHandler) {
