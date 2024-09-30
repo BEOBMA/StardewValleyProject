bbcd1f65e12d4de7c80e4f91cf696dd8a32e70f6
BEOBMA <ssdssdtkfk@naver.com>
2024-10-01 02:53:48 +0900

    feat:  광산 매니저 클래스 추가.  구체적 기능은 건축 완료 후 진행하는 편이 효율적일 듯 함.

--- src/main/java/org/beobma/stardewvalleyproject/manager/MineManager.kt
+++ src/main/java/org/beobma/stardewvalleyproject/manager/MineManager.kt
@@ -1,4 +1,19 @@
 package org.beobma.stardewvalleyproject.manager
 
-class MineManager {
+interface MineHandler {
+    fun reset()
+}
+
+class DefaultMineManager : MineHandler {
+    override fun reset() {
+        // 하루가 넘어가거나, 기타 이유로 광산 내부를 리셋해야할 경우
+    }
+}
+
+object MineManager {
+    private val converter: MineHandler = DefaultMineManager()
+
+    fun reset() {
+        converter.reset()
+    }
 }
