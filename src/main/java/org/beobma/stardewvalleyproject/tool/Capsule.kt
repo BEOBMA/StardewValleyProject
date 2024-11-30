e0dc6718d1e8e72d814251ba987d9f620713d2a1
BEOBMA <ssdssdtkfk@naver.com>
2024-12-01 06:01:14 +0900

    캡슐 시스템 임시 추가

---
+++ src/main/java/org/beobma/stardewvalleyproject/tool/Capsule.kt
@@ -0,0 +1,36 @@
+package org.beobma.stardewvalleyproject.tool
+
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class Capsule {
+    val weedKillerCapsule = ItemStack(Material.PAPER, 1).apply {
+        itemMeta = itemMeta.apply {
+            displayName(MiniMessage.miniMessage().deserialize("제초 캡슐"))
+            lore(
+                listOf(MiniMessage.miniMessage().deserialize("<gray>제초제 성분이 담긴 캡슐, 뿌리면 N일 동안은 경작지에서 잡초가 자라지 않는다."))
+            )
+        }
+    }
+
+    val growthCapsule = ItemStack(Material.PAPER, 1).apply {
+        itemMeta = itemMeta.apply {
+            displayName(MiniMessage.miniMessage().deserialize("성장 캡슐"))
+            lore(
+                listOf(MiniMessage.miniMessage().deserialize("<gray>성장 촉진제 성분이 담긴 캡슐, 수확일 까지 시간을 줄여준다."))
+            )
+        }
+    }
+
+    val nutrientCapsule = ItemStack(Material.PAPER, 1).apply {
+        itemMeta = itemMeta.apply {
+            displayName(MiniMessage.miniMessage().deserialize("영양 캡슐"))
+            lore(
+                listOf(MiniMessage.miniMessage().deserialize("<gray>비료 성분이 담긴 캡슐, 더 높은 등급의 작물이 나올 확률이 증가한다."))
+            )
+        }
+    }
+
+    val capsules = hashSetOf<ItemStack>(weedKillerCapsule, nutrientCapsule, weedKillerCapsule)
+}
